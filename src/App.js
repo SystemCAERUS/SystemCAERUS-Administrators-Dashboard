f0b209048c33d1435e4f2fb1136c0d3c4617839c
Janith Lahiru Kariyawasam <88109775+janithlahirukariyawasam@users.noreply.github.com>
2023-03-04 08:20:18 +0530

    Added Login and Home Pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import {
+  Route,
+  BrowserRouter,
+  Routes,
+} from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      mfewenwn
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
