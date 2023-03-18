916b0e12b34639f3eefc890731fb334322e2afa0
Janith Lahiru Kariyawasam <88109775+janithlahirukariyawasam@users.noreply.github.com>
2023-03-18 12:13:37 +0530

    init

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Sidebar from '../../components/sidebar/Sidebar'
+import '../home/home.scss'
+import Navbar from '../../components/navbar/Navbar'
 
 const Login = () => {
   return (
     <div className='login'>
+    <Sidebar/>
+    <div className="homeContainer">
+      <Navbar/>
       Login
     </div>
+  </div>
   )
 }
 
