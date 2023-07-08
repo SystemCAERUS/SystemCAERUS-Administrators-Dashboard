44726df0350d284bec7c148e023648d3887fc2d0
Janith Lahiru Kariyawasam <88109775+janithlahirukariyawasam@users.noreply.github.com>
2023-07-08 13:01:17 +0530

    add machine section

--- src/pages/Machinery/Machines.jsx
+++ src/pages/Machinery/Machines.jsx
@@ -1,18 +1,76 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "../Dashboard/home.scss";
-import Navbar from "../../components/navbar/Navbar";
+import MachineBox from "./components/MachineBox";
+import './machine.scss'
 
-function Machines() {
+function Machine() {
+  const [machines, setMachines] = useState([]);
+  const [selectedMachine, setSelectedMachine] = useState(null);
+
+  useEffect(() => {
+    const fetchMachines = async () => {
+      try {
+        const res = await axios.get("http://localhost:8800/machines");
+        setMachines(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchMachines();
+  }, []);
+
+  const openModal = (machine) => {
+    setSelectedMachine(machine);
+  };
+
+  const closeModal = () => {
+    setSelectedMachine(null);
+  };
+
   return (
     <div className="home">
-    <Sidebar />
-    <div className="homeContainer">
-      <Navbar />
-      <div className="content">Machines</div>
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+        <div className="hrwrapper">
+          <div
+            className="contentMachines"
+            style={{ overflowY: "auto", maxHeight: "85vh" }}
+          >
+            {machines.map((item) => (
+              <div key={item.id} onClick={() => openModal(item)}>
+                <MachineBox
+                  image={item.image}
+                  name={item.machinename}
+                  department={item.departmentid}
+                  description={item.departmentdes}
+                  URL={item.image}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      {selectedMachine && (
+        <div className="modal-overlay">
+          <div className="modal">
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
+            <h2>{selectedMachine.machinename}</h2>
+            <img src={selectedMachine.image} alt="" />
+            <p>{selectedMachine.description}</p>
+            <a href={selectedMachine.URL}>More details</a>
+          </div>
+        </div>
+      )}
     </div>
-  </div>
-  )
+  );
 }
 
-export default Machines
+export default Machine;
+
+
---
+++ src/pages/Machinery/components/MachineBox.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./machineBox.scss";
+
+function MachineBox(props) {
+  return (
+    <div className="machineBoxContent">
+      <div className="machineBoximage">
+        <img src={props.image} alt="" className="machineImage" />
+      </div>
+      <div className="machineContent">
+        <div className="machineBoxName">{props.name}</div>
+        <div className="machineBoxDepartment">{props.department}</div>
+        <div className="machineBoxDescription">{props.description}</div>
+        <div className="machineBoxURL">{props.URL}</div>
+      </div>
+    </div>
+  );
+}
+
+export default MachineBox;
